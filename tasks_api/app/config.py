6db68d49d5786ded9891d8f772d7b1b7d6063fe6
AmirNamdar <90144822+AmirNamdar@users.noreply.github.com>
2023-07-09 02:10:55 +0300

    load postgres env vars

--- tasks_api/app/config.py
+++ tasks_api/app/config.py
@@ -10,6 +10,14 @@ logger = logging.getLogger(__name__)
 class Settings(BaseSettings):
     otlp_exporter_url: str = os.getenv("OTLP_EXPORTER_URL", "")
 
+    pg_user: str = os.getenv("POSTGRES_USER", "")
+    pg_pass: str = os.getenv("POSTGRES_PASSWORD", "")
+    pg_host: str = os.getenv("POSTGRES_HOST", "")
+    pg_database: str = os.getenv("POSTGRES_DB", "")
+    asyncpg_url: str = (
+        f"postgresql+asyncpg://{pg_user}:{pg_pass}@{pg_host}:5432/{pg_database}"
+    )
+
 
 @lru_cache()
 def get_settings():
